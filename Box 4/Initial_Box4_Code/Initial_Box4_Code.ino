985764c6058112cbe3da86a08cc9a1bb9fce9ecd
Anishsan Gowrythasan <anishsan27@gmail.com>
2022-10-04 23:25:47 +1100

    Modified Code and Updated Pins to Match Latest Circuit

--- Box 4/Initial_Box4_Code/Initial_Box4_Code.ino
+++ Box 4/Initial_Box4_Code/Initial_Box4_Code.ino
@@ -1,83 +1,78 @@
 #include <Adafruit_NeoPixel.h>
 #include <Servo.h> 
 
+int topSensorState = 0;
+int topSensorLastState = HIGH;
+int topSensorPin  = 12;
 
-Servo myServo;
-
-int sensorState = 0;
-int lastState = HIGH;
-int sensorPin = 7;
-
-Servo servo_10;
-int topServoPin = 2;
+Servo topServo;
+int topServoPin = 13;
 int topServoPos = 1; //keeps incrementing everytime the button is pressed and the lights change
 
-int stripPin = 4;
-int numStripPixels = 32;    // change this value to however many pixels we have in the strip
-int lightSequence = 0;
+int spiralStripPin = 5;
+int spiralNumStripPixels = 32; // change this value to however many pixels we have in the strip
+int spiralLightSequence = 0;
 
-
-Adafruit_NeoPixel pixels(numStripPixels, stripPin, NEO_GRB + NEO_KHZ800);
-
+Adafruit_NeoPixel spiralStrip(spiralNumStripPixels , spiralStripPin , NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  pinMode(sensorPin, INPUT);
-  digitalWrite(sensorPin, HIGH);
-  pixels.begin();
-  servo_10.attach(10, 500, 2500);
+  pinMode(topSensorPin, INPUT);
+  digitalWrite(topSensorPin, HIGH);
+  spiralStrip.begin();
+//  topServo.write(0);
+  topServo.attach(topServoPin);
   
   Serial.begin(9600);
 }
 
 void loop() {
-  sensorState = digitalRead(sensorPin);
+  topSensorState = digitalRead(topSensorPin);
 
-  if (sensorState == LOW && lastState == HIGH) {
+  if (topSensorState == LOW && topSensorLastState == HIGH) {
     Serial.println("Ball Passed Through Sensor");
     changeTopStripCol();
   }
 
-  lastState = sensorState;
+  topSensorLastState = topSensorState;
 }
 
 void changeTopServo(int topServoPos){
   if(topServoPos % 2 == 0){ //for every second ball, the servo will rotate 90 degrees 
-      servo_10.write(90);
+      topServo.write(90);
     }
   if(topServoPos % 2 != 0){
-      servo_10.write(0);
+      topServo.write(0);
     }
-
 }
 
 void changeTopStripCol() {
-  if (lightSequence == 0) {
+  if (spiralLightSequence  == 0) {
     changeTopServo(topServoPos);
-    for (int i = 0; i < numStripPixels; i++) {
-      pixels.setPixelColor(i, pixels.Color(random(1, 254), 0, 0));
-      pixels.show();
+    for (int i = 0; i < spiralNumStripPixels ; i++) {
+      spiralStrip.setPixelColor(i, spiralStrip.Color(255, 0, 0));
+      spiralStrip.show();
     }
-    lightSequence ++;
+    spiralLightSequence++;
     topServoPos++;
   }
   
-  else if (lightSequence == 1) {
+  else if (spiralLightSequence  == 1) {
     changeTopServo(topServoPos);
-    for (int i = 0; i < numStripPixels; i++) {
-      pixels.setPixelColor(i, pixels.Color(0, random(1, 254), 0));
-      pixels.show();
+    for (int i = 0; i < spiralNumStripPixels ; i++) {
+      spiralStrip.setPixelColor(i, spiralStrip.Color(0, 255, 0));
+      spiralStrip.show();
     }
-    lightSequence ++;
+    spiralLightSequence++;
     topServoPos++;
   }
   
   else {
     changeTopServo(topServoPos);
-    for (int i = 0; i < numStripPixels; i++) {
-     pixels.setPixelColor(i, pixels.Color(0, 0, random(1, 254)));
-      pixels.show();
+    for (int i = 0; i < spiralNumStripPixels ; i++) {
+     spiralStrip.setPixelColor(i, spiralStrip.Color(0, 0, 255));
+     spiralStrip.show();
     }
-    lightSequence =0;
+    spiralLightSequence = 0;
     topServoPos++;
   }
 }
