629ddd96a1c0e194417b011ec0d86fbc1a268a8d
Bev7787 <eregister2me@gmail.com>
2022-10-05 18:31:25 +1100

    Modify motorsenstate to runmotor to better reflect functionality. Change order of functions to put all functions above loop(). Resolve error in pin change interrupt variable. Convert motor sensors to INPUT_PULLUP and convert ball sensors to initialise at LOW and trigger at HIGH.

--- Box 3/box3.ino
+++ Box 3/box3.ino
@@ -10,19 +10,16 @@ unsigned long motortimestamp1 = 0;
 unsigned long motortimestamp2 = 0;
 unsigned long balltimestamp1 = 0;
 unsigned long balltimestamp2 = 0;
-bool motorsenstate1 = false;
-bool motorsenstate2 = false;
+bool runmotor1 = false;
+bool runmotor2 = false;
 
 bool retraceLightLeft = false;
 bool retraceLightRight = false;
 // bool spiralLed = false;
 
-int lastState1 = HIGH;
-int lastState2 = HIGH;
-
 // ball sensor pins placeholder
-int ballPin1 = 8;
-int ballPin2 = 9;
+int ballPin1 = A0;
+int ballPin2 = A1;
 
 // Motor A connections
 int enA = 1;
@@ -47,19 +44,18 @@ LinkedList<Ball>
 
 LinkedList<Ball> ballsInSpiral = LinkedList<Ball>(); // spiral path queue
 
+
 void setup()
 {
   // Set sensors
-  pinMode(motorSen1, INPUT);
-  pinMode(motorSen2, INPUT);
+  pinMode(motorSen1, INPUT_PULLUP);
+  pinMode(motorSen2, INPUT_PULLUP);
   pinMode(ballPin1, INPUT);
   pinMode(ballPin2, INPUT);
 
   // Sensor modes set
-  digitalWrite(motorSen1, HIGH);
-  digitalWrite(motorSen2, HIGH);
-  digitalWrite(ballPin1, HIGH);
-  digitalWrite(ballPin2, HIGH);
+  digitalWrite(ballPin1, LOW);
+  digitalWrite(ballPin2, LOW);
 
   // Set all the motor control pins to outputs
   pinMode(enA, OUTPUT);
@@ -73,9 +69,13 @@ void setup()
   analogWrite(enA, 255);
   analogWrite(enB, 255);
 
+  // Motor off by default
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+
   // Setup marble sensor interrupts.
   PCICR |= B00000010;
-  PMSK1 |= B00000011;
+  PCMSK1 |= B00000011;
 
   // Attach reed switch interrupts
   attachInterrupt(digitalPinToInterrupt(motorSen1), leftRetracePath, RISING);
@@ -84,54 +84,58 @@ void setup()
 
 ISR(PCINT1_vect)
 {
-  if (digitalRead(ballPin1) == LOW)
+  if (digitalRead(ballPin1) == HIGH)
   {
     // set boolean
     balltimestamp1 = millis();
-    motorsenstate1 = true;
+    runmotor1 = true;
   }
-  if (digitalRead(ballPin2) == LOW)
+  if (digitalRead(ballPin2) == HIGH)
   {
     // set boolean
     balltimestamp2 = millis();
-    motorsenstate2 = true;
+    runmotor2 = true;
   }
 }
 
 void leftRetracePath()
 {
-  motorsenstate1 = false;
+  runmotor1 = false;
   retraceLightLeft = true;
   Ball bl = {millis()};
-  ballsInSystemQueueL.add(bl);
+  //ballsInSystemQueueL.add(bl);
 }
 
 void rightRetracePath()
 {
-  motorsenstate2 = false;
+  runmotor2 = false;
   retraceLightRight = true;
   Ball br = {millis()};
-  ballsInSystemQueueR.add(br);
+  //ballsInSystemQueueR.add(br);
+}
+
+void runMotor(int in1, int in2, bool runmotor)
+{
+  if (runmotor == true)
+  {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+  }
+  else
+  {
+    // Turn off motors.
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+  }
 }
 
 void loop()
 {
-  /*int ballSen1State = digitalRead(ballPin1);
-  int ballSen2State = digitalRead(ballPin2);
 
-  if (ballSensor1 == LOW && lastState1 == HIGH)
-    motortimestamp1 = millis() + 1000;
-
-  else if (ballSensor2 == LOW && lastState2 == HIGH)
-    motortimestamp2 = millis() + 1000;
-  lastState1 = ballSen1State;
-  lastState2 = ballSen2State;
-  */
-  // TODO: Implement interrupts
-  if ((unsigned long)(millis() - balltimestamp1) >= 1000)
-    runMotor(in1, in2, motorsenstate1);
-  if ((unsigned long)(millis() - balltimestamp2) >= 1000)
-    runMotor(in3, in4, motorsenstate2);
+  if ((millis() - balltimestamp1) >= 1000)
+    runMotor(in1, in2, runmotor1);
+  if ((millis() - balltimestamp2) >= 1000)
+    runMotor(in3, in4, runmotor2);
 
   if (retraceLightLeft)
   {
@@ -139,6 +143,7 @@ void loop()
     Ball bl = ballsInSystemQueueL.get(0);
     if (millis() - bl.reedSwitchTimestamp >= RETRACE_TIME)
     {
+      // TODO: LED left func
       retraceLightLeft = false;
       Ball removedBall = ballsInSystemQueueL.remove(0);
       ballsInSpiral.add(removedBall);
@@ -151,6 +156,7 @@ void loop()
     Ball br = ballsInSystemQueueR.get(0);
     if (millis() - br.reedSwitchTimestamp >= RETRACE_TIME)
     {
+      // TODO: LED right func
       retraceLightRight = false;
       Ball removedBall = ballsInSystemQueueR.remove(0);
       ballsInSpiral.add(removedBall);
@@ -162,23 +168,9 @@ void loop()
     Ball bl = ballsInSpiral.get(0);
     if (millis() - bl.reedSwitchTimestamp >= SPIRAL_TIME)
     {
+      // TODO: LED spiral func
       ballsInSpiral.remove(0);
     }
   }
 }
 
-void runMotor(int in1, int in2, bool motorsen)
-{
-  // TODO: Replace motorsensor and ballsensor with interrupts.
-  if (motorsen == true)
-  {
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-  }
-  else
-  {
-    // Turn off motors.
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-  }
-}
